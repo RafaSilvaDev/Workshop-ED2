1b8e41ee204d764fec819718670271eefb8bbd3b
rafasilvadev <rafabdasilva12@gmail.com>
2022-10-10 13:20:06 -0300

    Em desenvolvimento

--- src/test/java/tests/SetUtilsTests.java
+++ src/test/java/tests/SetUtilsTests.java
@@ -6,17 +6,27 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.commons.collections4.SetUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 // https://www.baeldung.com/apache-commons-setutils
 
 class SetUtilsTests {
-	Set<Integer> a, b, esperado;
+	Set<Integer> a, b;
 	
 	@BeforeEach
 	void inicializar() {
-		a = new HashSet(Arrays.asList(1, 2, 5));
+		a = new HashSet<Integer>(Arrays.asList(1, 2, 5));
+		b = new HashSet<Integer>(Arrays.asList(1, 2));
+	}
+	
+	@Test
+	void diferencaEntreSets() {
+		SetUtils.SetView<Integer> result = SetUtils.difference(a, b);
+		
+		assertTrue(result.size() == 1);
+		assertTrue(result.contains(5));
 	}
 
 }
